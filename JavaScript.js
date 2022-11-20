be54f991fe207e38332bbb9bac9bb66e0f243bd7
HP <sochor5@stud.uniza.sk>
2022-11-20 21:37:45 +0100

    1.6   js

--- JavaScript.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    var validEmail = document.getElementById("email").value.match(emailRegex);
-    if (validEmail == null) {
-        alert("Váš mail je chybný. Správny tvar: mail@domena.domena");
-        return false;
-    }
-}
--- login/NewAccount.php
+++ login/NewAccount.php
@@ -1,5 +1,6 @@
 <?php
 include "DBuser.php";
+
 $auth = new DBuser();
 ?>
 <html lang="sk">
@@ -45,7 +46,14 @@ $auth = new DBuser();
     </footer>
 </div>
 <script>
-
+    function validateForm() {
+        var emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        var validEmail = document.getElementById("email").value.match(emailRegex);
+        if (validEmail == null) {
+            alert("Váš mail je chybný. Správny tvar: mail@domena.domena");
+            return false;
+        }
+    }
 </script>
 </body>
 </html>
